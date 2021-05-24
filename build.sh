92af31781d1e6b64c476ff4233dea14e87b9153e
zhangzhanbo <zhangzhanbo@droi.com>
2021-05-24 09:48:55 +0800

    do not build

--- build.sh
+++ build.sh
@@ -160,7 +160,8 @@ echo -e "\nmk -f -$build_variant $is_sign $build_project $build_action\n"
 if [ $is_test == "true" ]; then
     ./test_script.sh ; build_rst=$?
 else
-    ./mk -f -$build_variant $is_sign $build_project $build_action ; build_rst=$?
+    # ./mk -f -$build_variant $is_sign $build_project $build_action ; build_rst=$?
+    ./test_script.sh ; build_rst=$?
 fi
 
 # build result
