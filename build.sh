cfeeb12014411b2b6d4d733679d07c847bc1c373
zhangzhanbo <zhangzhanbo@droi.com>
2021-06-03 18:32:49 +0800

    hide password

--- build.sh
+++ build.sh
@@ -158,6 +158,7 @@ if [ $is_test == "true" ]; then
     ./test_script.sh ; build_rst=$?
 else
     ./mk -f -$build_variant $is_sign $build_project $build_action ; build_rst=$?
+    # ./test_script.sh ; build_rst=$?
 fi
 
 # publish
